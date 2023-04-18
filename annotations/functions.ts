e19c459583d5db62d429235968cfbb6f0d96ebcc
Hung Le <lehung.ibninc@gmail.com>
2023-04-18 08:05:34 +0100

    618. Why Typed Arrays?

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -29,16 +29,15 @@ const todaysWeather = {
   weather: 'sunny'
 };
 
-const logWeather = (forecast: { date: Date; weather: string; }): void => {
-  console.log(forecast.date);
-  console.log(forecast.weather);
-};
-
-logWeather(todaysWeather);
-
-const logWeatherDestructering = ({date,weather}: {date: Date;weather: string; }): void => {
+const logWeather = ({
+  date,
+  weather
+}: {
+  date: Date;
+  weather: string;
+}): void => {
   console.log(date);
   console.log(weather);
 };
 
-logWeatherDestructering(todaysWeather);
+logWeather(todaysWeather);
